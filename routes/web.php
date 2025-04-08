2d5ec496402d7b596ff2731e06a1e4d9f8c58454
Nguyễn Trung Sang <sangntpc08899@gmail.com>
2025-04-08 12:35:13 +0700

    newcode 0804

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,20 @@
 <?php
 
-use App\Http\Controllers\Client\HomeController;
-use App\Http\Controllers\Client\ProductController;
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/',[HomeController::class, 'index']);
-Route::get('/chi-tiet-san-pham/{slug}',[ProductController::class, 'detail']);
-Route::get('/dm-san-pham/{id}',[ProductController::class, 'categories']);
-
-
-
-Route::get('/nguoi-dung', function(){
-    $query=DB::table('users')
-    ->select('id','name')
-    ->orderBy('id' ,'desc');
-    $data = $query->get();
-    foreach($data as $user) echo "<p>{$user->name}</p>";
-
-
+Route::get('/', function () {
+    return view('welcome');
 });
-Route::get('/sinh-vien', function(){
-    $query=DB::table('students')
-    ->join('courses', 'students.course_id', '=', 'courses.id')
-    ->select('students.name as name','students.code as code', 'courses.name as khoa');
-    $data = $query->get();
-    foreach($data as $std) echo "<p>{$std->code} - {$std->name} - {$std->khoa}</p>";
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
